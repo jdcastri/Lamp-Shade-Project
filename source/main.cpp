0c7721b0784f4d9552556d48bab2b66bd41e2ce8
Juan D. Castrillon <jdcastri@mit.edu>
2015-04-26 00:57:35 -0400

    Added ability to open and read pixel value of image

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,12 @@
 #include "../include/CompFab.h"
 #include "../include/Mesh.h"
 
+//#include "opencv2/core/core_c.h"
+#include "opencv2/core/core.hpp"
+//#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 //Ray-Triangle Intersection
 //Returns 1 if triangle and ray intersect, 0 otherwise
 int rayTriangleIntersection(CompFab::Ray &ray, CompFab::Triangle &triangle)
@@ -185,19 +191,35 @@ int main(int argc, char **argv)
     
     CompFab::Vec3 hspacing(0.5*spacing, 0.5*spacing, 0.5*spacing);
     
-    for (int ii = 0; ii < nx; ii++) {
-        for (int jj = 0; jj < ny; jj++) {
-            for (int kk = 0; kk < nz; kk++) {
-                CompFab::Vec3 coord(((double)ii)*spacing, ((double)jj)*spacing, ((double)kk)*spacing);
-                voxelPos = coord + hspacing;
-                int hits = numSurfaceIntersections(voxelPos, direction);
+    // for (int ii = 0; ii < nx; ii++) {
+    //     for (int jj = 0; jj < ny; jj++) {
+    //         for (int kk = 0; kk < nz; kk++) {
+    //             CompFab::Vec3 coord(((double)ii)*spacing, ((double)jj)*spacing, ((double)kk)*spacing);
+    //             voxelPos = coord + hspacing;
+    //             int hits = numSurfaceIntersections(voxelPos, direction);
 
-                if (hits % 2 != 0) {
-                    g_voxelGrid->isInside(ii,jj,kk) = true;
-                }
-            }
-        }
-    }
+    //             if (hits % 2 != 0) {
+    //                 g_voxelGrid->isInside(ii,jj,kk) = true;
+    //             }
+    //         }
+    //     }
+    // }
+
+    using namespace cv;
+    using namespace std;
+
+    // Reads image at path.
+    Mat img = imread("/home/jdcastri/Dropbox (MIT)/Spring 2015/6.S079/project/input/images/default_square.png",CV_LOAD_IMAGE_GRAYSCALE);
+    // intensity is value at (y,x) coordinate of image
+    Scalar intensity = img.at<uchar>(220, 140);
+    // intensity[0] is value between (0,255) for pixel
+    // 0 is black. 255 is white. 
+    cout << intensity[0] << endl;
+
+    // show a window with the image. For testing purposes.
+    imshow("hello", img);
+    // halts code
+    waitKey(0);
 
     //Write out voxel data as obj
     saveVoxelsToObj(argv[2]);
